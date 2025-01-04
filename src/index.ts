dc6c07dd81b8aaeb869ae2a4ae9bc26a8428ee2c
yanis900 <yanis991@hotmail.com>
2025-01-04 23:05:19 +0000

    rename function and update logic to scrape car data

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import puppeteer from "puppeteer";
 
-const getCompanies = async (url: string) => {
+const getCarData = async (url: string) => {
   const browser = await puppeteer.launch({
     // start Puppeteer session.
     headless: true, // use visible browser
@@ -14,18 +14,24 @@ const getCompanies = async (url: string) => {
     waitUntil: "domcontentloaded", // wait for content to load before appearing
   });
 
-  const companies = await page.evaluate(() => {
-    const company = document.querySelectorAll("ul > li > a:first-child");
+  const carData = await page.evaluate(() => {
+    // const company = document.querySelectorAll("ul > li > a:first-child");
+    // return Array.from(company).map((e) => e.textContent);
+    const title = document.querySelector(".vehicleCard__title");
+    const specs = document.querySelector(".vehicleCard__uspList");
+    const price = document.querySelector(".vehicleCard__price__inner");
 
-    return Array.from(company).map((e) => e.textContent); 
+    // return Array.from(title).map((e) => e.textContent?.trim());
+    return [
+      title?.textContent?.trim(),
+      specs?.textContent?.trim(),
+      price?.textContent?.trim(),
+    ];
   });
 
-  console.dir(companies, { maxArrayLength: null }); // log the entire array
-
+  console.dir(carData, { maxArrayLength: null }); // log the entire array
   // close browser
   await browser.close();
 };
 
-getCompanies(
-  "https://en.wikipedia.org/wiki/List_of_companies_of_the_United_Kingdom_A%E2%80%93J"
-);
+getCarData("https://www.saxton4x4.co.uk/");
